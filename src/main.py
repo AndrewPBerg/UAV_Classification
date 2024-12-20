de86ee8afbe69cdd03bcd45323ba6bb5e5adeb0a
Andrew Berg <andberg9@gmail.com>
2024-12-19 20:28:39 -0500

    init main.py

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from script import main as script_main
+from sweeps import main as sweep_main
+import yaml
+
+def alter_config_key(file_path: str, key:str, value):
+
+
+def main():
+    script_main()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
