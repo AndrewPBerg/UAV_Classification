9d2a2f097bc2dfae911f65b99461084b132e2b9d
AndrewPBerg <andberg9@gmail.com>
2025-03-06 07:20:20 -0500

    init

--- src/models/cnn_models.py
+++ src/models/cnn_models.py
@@ -86,14 +86,16 @@ class CNNModel:
             raise ValueError(f"Unsupported MobileNet model type: {model_type}")
         
         # Modify first convolutional layer to accept grayscale input
-        if input_shape[0] == 1:
-            model.features[0][0] = nn.Conv2d(
-                1, 16, kernel_size=3, stride=2, padding=1, bias=False
-            )
+
+        model.features[0][0] = nn.Conv2d(
+            1, 16, kernel_size=3, stride=2, padding=1, bias=False
+        )
         
         # Replace classification head
         in_features = model.classifier[-1].in_features
         model.classifier[-1] = nn.Linear(in_features, num_classes)
+        
+        model = apply_peft(model, peft_config)
         
         return model
     
@@ -121,18 +123,20 @@ class CNNModel:
             raise ValueError(f"Unsupported EfficientNet model type: {model_type}")
         
         # Modify first convolutional layer to accept grayscale input
-        if input_shape[0] == 1:
-            model.features[0][0] = nn.Conv2d(
-                1, model.features[0][0].out_channels,
-                kernel_size=model.features[0][0].kernel_size,
-                stride=model.features[0][0].stride,
-                padding=model.features[0][0].padding,
-                bias=False
-            )
-        
+
+        model.features[0][0] = nn.Conv2d(
+            1, model.features[0][0].out_channels,
+            kernel_size=model.features[0][0].kernel_size,
+            stride=model.features[0][0].stride,
+            padding=model.features[0][0].padding,
+            bias=False
+        )
+    
         # Replace classification head
         in_features = model.classifier[-1].in_features
         model.classifier[-1] = nn.Linear(in_features, num_classes)
+        
+        model = apply_peft(model, peft_config)
         
         return model
 
